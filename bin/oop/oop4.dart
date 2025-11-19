f148a1695632e6401cc57c6c94614104f22224a9
Fachmi Ramadhan <Fachmi@citra.co.id>
2025-11-19 16:45:55 +0700

    belajar oop part-7

--- bin/oop/oop4.dart
+++ bin/oop/oop4.dart
@@ -1,3 +1,7 @@
+import 'data/category.dart';
+import 'data/location.dart';
+
+
 /** Object Class 
  * setiap class yang kita buat secara automatis adalah turunan dari object, kecuali null
  * walaupun tidak secara langsung kita eksplisit menyebutkan extends object, tapi secara otomatis akan membuat class kita extends object
@@ -40,19 +44,51 @@ class staff extends manager{
 
 
 
+// void main(){
+//   manager manager1 = supervisor("Andi");
+//   manager manager2 = staff("Budi");
+
+//   print("Manager 1: ${manager1.name}");
+//   print("Manager 2: ${manager2.name}");
+
+//   manager manager3 = manager("Charlie");
+//   print(manager3);
+
+//   manager3 = supervisor("Diana");
+//   print("Manager 3: ${manager3.name}");
+
+//   manager3 = staff("Evan");
+//   print("Manager 3: ${manager3.name}");
+// }
+
+
+/**Import 
+ * import adalah kemampuan untuk menggunakan class, fucntion atau variable yang berada difile yang berbeda
+ * import sangan cocok digunakan ketika kode program kita sudah banyak dan ditempatkan di banyak file 
+ */
+
+//  void main(){
+//   var category = Category('1', 'laptop');
+//   print(category.id);
+//   print(category.name);
+//  }
+
+/**abstract class 
+ * saat kita membuat class, kita bisa menjadikan sebuah class sebagai abstract class 
+ * abstract class artinya, class tersebut tidak bisa dibuat sebagai object secara langsung, hanya bisa diturunkan
+ * untuk membuat sebuah class menjadi abstract, kita bisa menggunakan kata kunci abstract sebelum kata kunci class 
+ * dengan demikian abstract class bisa kita gunakan sebagai kontrak untuk class
+ */
+
 void main(){
-  manager manager1 = supervisor("Andi");
-  manager manager2 = staff("Budi");
+  var city = City('sumedang');
 
-  print("Manager 1: ${manager1.name}");
-  print("Manager 2: ${manager2.name}");
-
-  manager manager3 = manager("Charlie");
-  print(manager3);
-
-  manager3 = supervisor("Diana");
-  print("Manager 3: ${manager3.name}");
-
-  manager3 = staff("Evan");
-  print("Manager 3: ${manager3.name}");
+  // var location = Location(); //error
 }
+
+/** Abstarct Method 
+ * saat kita membuat class yang abstract, kita bisa membuat abstract method juga didalam abstract tersebut
+ * saat kita membuat sebuah abstract method, kita tidak boleh membua blok method tersebut
+ * artinya abstract method wajib dioverride di class child
+ */
+
